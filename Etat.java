e9566ca44508e967559c2d5766c919a84892f4d6
Collignon <valentin.collignon9@etu.univ-lorraine.fr>
2018-02-14 17:02:21 +0100

    mise en place des class absraite joueur et etat

--- Etat.java
+++ Etat.java
@@ -3,6 +3,8 @@
  */
 package othello;
 
+import java.util.ArrayList;
+
 /**
  * @author Collignon Valentin
  * @author Genard Pierre
@@ -16,5 +18,13 @@ public abstract class Etat {
 	public Etat() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public abstract ArrayList<Etat> successeurs ();
+	
+	public boolean equals(Etat e){
+		return this.toString().equals(e.toString());
+	}
+	
+	
 
 }
