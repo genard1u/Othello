cff1615f8da362719c04c7f4a06dc2e97c8becc0
Collignon <valentin.collignon9@etu.univ-lorraine.fr>
2018-02-14 17:18:08 +0100

    class EtatOthello

---
+++ EtatOthello.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package othello;
+
+import java.util.ArrayList;
+
+/**
+ * @author Collignon Valentin
+ * @author Genard Pierre
+ *
+ */
+public class EtatOthello extends Etat {
+
+	/**
+	 * 
+	 */
+	public EtatOthello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see othello.Etat#successeurs()
+	 */
+	@Override
+	public ArrayList<Etat> successeurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
